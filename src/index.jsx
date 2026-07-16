3b5e59a68d7d4a89dbe9fc5dee523eba5f47e3e5
Claude Seguret <clodio@users.noreply.github.com>
2026-07-16 10:16:24 +0200

    Add files via upload

--- src/index.jsx
+++ src/index.jsx
@@ -12,6 +12,6 @@ root.render(
 
 if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js");
+    navigator.serviceWorker.register("./service-worker.js");
   });
 }
