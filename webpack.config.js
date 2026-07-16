d6534d9c31b404513b138125e1f41b046bfda174
Claude Seguret <clodio@users.noreply.github.com>
2026-07-16 11:08:01 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,9 @@ module.exports = (_env, argv) => {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
         },
       },
       {
